793bc54d5e53f7923439584670cf88a59d7329cb
Kevin Pereira <kapforu@live.com>
2025-04-20 16:28:00 -0500

    mytinytodo-orig Initial Check-in 144x Files

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+// myTinyTodo Database connection configuration
+
+define('MTT_DB_TYPE', 'sqlite');
+
+define('MTT_DB_HOST', 'localhost');
+
+define('MTT_DB_USER', 'mtt');
+
+define('MTT_DB_PASSWORD', 'mtt');
+
+define('MTT_DB_NAME', 'mytinytodo');
+
+define('MTT_DB_PREFIX', '');
+
+define('MTT_DB_DRIVER', '');
+
+define('MTT_SALT', '6c142e5d-4bdb-4fdf-8bc7-97287b962eab');
